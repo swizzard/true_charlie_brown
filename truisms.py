24f919447cf96aa433f67ae7f16d1b4b6516aa2b
Sam Raker <sam.raker@gmail.com>
2014-12-31 16:10:19 -0500

    trying to fix generator, reformatting truisms to use newlines

--- truisms.py
+++ truisms.py
@@ -1,11 +1,12 @@
 # coding=utf8
 __author__ = 'Sam Raker'
 
-import re
 
 def gen():
     with open("truisms.txt") as f:
-        txt = f.read()
-        truisms = [x for x in re.split(r'",?', txt) if x] 
-    for x in truisms:
-        yield "{}, Charlie Brown".format(x.title())
+        i = iter(f.readlines())
+    while True:
+        try:
+            yield "{}, Charlie Brown".format(i.next().rstrip().title())
+        except StopIteration:
+            break
