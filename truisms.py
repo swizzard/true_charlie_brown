503ba0618a8d647dde488e997dd9fd2b1ca23476
Sam Raker <sam.raker@gmail.com>
2015-06-15 19:57:23 -0400

    dogfoodin

--- truisms.py
+++ truisms.py
@@ -1,7 +1,12 @@
 # coding=utf8
 __author__ = 'Sam Raker'
 
+from twitter.oauth import OAuth
 
+from botmaster import gen_tweet
+from miniconfig import config
+
+@gen_tweet(auth=OAuth(**config), interval=1800)
 def gen():
     with open("truisms.txt") as f:
         i = iter(f.readlines())
@@ -10,3 +15,7 @@ def gen():
             yield "{}, Charlie Brown".format(i.next().rstrip().title())
         except StopIteration:
             break
+
+
+if __name__ == "__main__":
+    gen()
